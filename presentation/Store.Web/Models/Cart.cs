378e587f3f5916fc9285902e26af1563cbe86fa5
daamnwhaii <allaberdinilmir11@gmail.com>
2024-01-12 16:54:56 +0500

    Реализована возможность добавлять товары в корзину

---
+++ presentation/Store.Web/Models/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace Store.Web.Models
+{
+    public class Cart
+    {
+        public IDictionary<int, int> Items { get; set; } = new Dictionary<int, int>(); // int1 - id товара, int2 - кол-во товаров в корзине
+
+        public decimal Amount { get; set; }
+    }
+}
--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -10,6 +10,14 @@ namespace Store.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             builder.Services.AddSingleton<IItemRepository, ItemRepository>();
             builder.Services.AddSingleton<ItemService>();
 
@@ -29,6 +37,8 @@ namespace Store.Web
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.MapControllerRoute(
                 name: "default",
