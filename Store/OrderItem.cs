0f91edc29642b8eba378228ea411cd6ef125fc2c
Vladislav <youmiramistin@gmail.com>
2024-01-15 14:32:56 +0500

    Добавлен объект OrderItem

---
+++ Store/OrderItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class OrderItem
+    {
+        public int ItemId { get; }
+
+        public int Count { get; }
+
+        public decimal Price { get; }
+
+        public OrderItem(int itemId, int count, decimal price)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Count must be greater than zero.");
+            }
+
+            ItemId = itemId;
+            Count = count;
+            Price = price;
+        }
+    }
+}
